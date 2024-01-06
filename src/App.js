271236a4bbb96ee6ae1e382217993b998b8ecb37
CSunilReddy <csunilreddy3@gmail.com>
2024-01-06 14:01:28 +0530

    initial code commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Sunil
+      </header>
+    </div>
+  );
+}
+
+export default App;
